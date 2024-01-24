1233844aa986b520d227ac4ff134d4cc58c6a969
llc1234 <93080319+llc1234@users.noreply.github.com>
2024-01-24 14:38:53 +0100

    Update BlackNova.py

--- BlackNova.py
+++ BlackNova.py
@@ -3,9 +3,12 @@ import colorama
 
 colorama.init()
 
+
 class BlackNova:
     def __init__(self):
         self.running = True
+
+        self.use_data = {}
 
         self.start()
 
@@ -49,7 +52,8 @@ class BlackNova:
         found = False
         for pp in file_list:
             row = pp.find(command[1])
-            if not row == -1:
+            row_txt = pp.find("info.txt")
+            if not row == -1 and row_txt == -1:
                 st = colorama.Fore.WHITE + pp.replace(command[1], f"{colorama.Back.RED}{command[1]}{colorama.Fore.WHITE}{colorama.Back.RESET}")
                 print(st.replace("\\", "/"))
                 found = True
@@ -60,8 +64,74 @@ class BlackNova:
             print("")
         print("")
 
-    def use_BlackNova(self, command):
-        os.system(f"python {command[1]}")
+    def set_command(self, command):
+        try:
+            for i in range(len(command)):
+                try:
+                    t = self.use_data[command[1].lower()]
+                    self.use_data[command[1].lower()] = command[2]
+                except:
+                    pass
+        except:
+            pass
+
+    def exploit_run_command(self, dir):
+        ad = ""
+        for name, inside in self.use_data.items():
+            ad += inside + " "
+
+        print(f"python {dir} {ad}")
+        # os.system(f"python {dir} {ad}")
+
+    def help_command(self):
+        print("")
+        print(self.help_command_text)
+        print("")
+
+    def show_options_command(self):
+        print("")
+        for name, inside in self.use_data.items():
+            print(f"{name}: {inside}")
+        print("")
+
+
+    def get_info_txt(self, dir):
+        path = os.path.dirname(dir)
+
+        print(f"{path}/info.txt")
+
+        r = open(f"{path}/info.txt", "r")
+        
+        for pp in r.read().split(":"):
+            self.use_data[pp] = ""
+
+        r.close()
+
+
+    def use_BlackNova(self, comm):
+        # os.system(f"python {command[1]}")
+
+        self.get_info_txt(comm[1])
+
+        print("")
+        print(f"Start {comm[1]}")
+        print("")
+
+        while True:
+            comman = input(f"m9>").split(" ")
+            command = [item for item in comman if item != '']
+
+            if command[0].lower() == "exit":
+                self.use_data = {}
+                break
+            if command[0].lower() == "help":
+                self.help_command()
+            elif command[0].lower() == "set":
+                self.set_command(command)
+            elif command[0].lower() == "show" and command[1].lower() == "options":
+                self.show_options_command()
+            elif command[0].lower() == "run" or command[0].lower() == "exploit":
+                self.exploit_run_command(comm[1])
 
     def start(self):
         while self.running:
